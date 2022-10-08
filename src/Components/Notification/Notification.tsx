9900de2f61f82d324b5d6eb1703f36f16a5a762c
Diego <92130153+DiegoPlaninscheck@users.noreply.github.com>
2022-10-08 00:55:23 -0300

    feat: insert media query typography

--- src/Components/Notification/Notification.tsx
+++ src/Components/Notification/Notification.tsx
@@ -1,48 +1,65 @@
-import { SvgIconTypeMap } from "@mui/material";
+import { makeStyles, responsiveFontSizes, SvgIconTypeMap } from "@mui/material";
 import { OverridableComponent } from "@mui/material/OverridableComponent";
 import { Box } from "@mui/system";
-import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded';
+import DeleteRoundedIcon from "@mui/icons-material/DeleteRounded";
 import Typography from "@mui/material/Typography";
+import "./Notification.scss";
 
 export default function Notification(props: {
-    Icone: OverridableComponent<SvgIconTypeMap<{}, "svg">> & {
-        muiName: string;
-    }, titulo: string, mensagem: string
+  Icone: OverridableComponent<SvgIconTypeMap<{}, "svg">> & {
+    muiName: string;
+  };
+  titulo: string;
+  mensagem: string;
 }) {
-    return (
-        <>
-            <Box sx={{
-                width: "100%",
-                display: "flex",
-                justifyContent: "space-between",
-                alignItens: "center",
-                backgroundColor: "#DDDDDD",
-                borderRadius: "5px",
-                margin: 2
-            }}>
-                <Box sx={{ width: "40%", display: "flex", justifyContent: "space-around", alignItems: "center" }}>
-                    <Box>
-                        <props.Icone sx={{ color: "#595959" }}></props.Icone>
-                    </Box>
-                    <Box sx={{ marginRight: 5, "@media(maxWidth: 720px)": {fontSize: "12px"} }}>
-                        <div className="text">
-                        <Box>
-                            <Typography variant="h6" component="h1" >
-                                {props.titulo}
-                            </Typography>
-                        </Box>
-                        <Box>
-                            <Typography variant="body2" component="h2">
-                                {props.mensagem}
-                            </Typography>
-                        </Box>
-                        </div>
-                    </Box>
-                </Box>
-                <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", marginRight: 2 }}>
-                    <DeleteRoundedIcon sx={{ color: "#595959" }} />
-                </Box>
+  return (
+    <>
+      <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItens: "center",
+          backgroundColor: "#DDDDDD",
+          borderRadius: "5px",
+          margin: 2,
+        }}
+      >
+        <Box
+          sx={{
+            width: "40%",
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+          }}
+        >
+          <Box>
+            <props.Icone sx={{ color: "#595959" }}></props.Icone>
+          </Box>
+          <Box className="containerConteudo">
+            <Box>
+              <Typography className="h1" variant="h6" component="h1">
+                {props.titulo}
+              </Typography>
             </Box>
-        </>
-    )
+            <Box>
+              <Typography className="h2" variant="body2" component="h2">
+                {props.mensagem}
+              </Typography>
+            </Box>
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginRight: 2,
+          }}
+        >
+          <DeleteRoundedIcon sx={{ color: "#595959" }} />
+        </Box>
+      </Box>
+    </>
+  );
 }
---
+++ src/MediaQuery/index.tsx
@@ -0,0 +1,6 @@
+import Typography from "@mui/material/Typography";
+import useMediaQuery from "@mui/material/useMediaQuery";
+
+export default function MediaQuery() {
+  const media = useMediaQuery("(max-width: 720px)");
+}
