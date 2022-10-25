a5ba8afe86e41d91e76487547dbc961b40f80338
DiegoPlaninscheck <92130153+DiegoPlaninscheck@users.noreply.github.com>
2022-10-25 09:39:26 -0300

    fix: arrumando login

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -13,6 +13,7 @@ import AccountCircle from "@mui/icons-material/AccountCircle";
 import LockRoundedIcon from "@mui/icons-material/LockRounded";
 import RemoveRedEyeRoundedIcon from "@mui/icons-material/RemoveRedEyeRounded";
 import InputRoundedIcon from "@mui/icons-material/InputRounded";
+import { positions } from "@mui/system";
 
 function Login(props: {
   setAberto: React.Dispatch<React.SetStateAction<boolean>>;
@@ -28,26 +29,117 @@ function Login(props: {
 
   return (
     <>
-      <Box
-        id="login"
-      >
+      <Box id="login">
         <Box
           sx={{
             width: "100%",
             height: "100%",
             display: "flex",
-            justifyContent: "space-between"
+            justifyContent: "space-between",
           }}
         >
-          <Box sx={{
-            display: "flex",
-            justifyContent: "start",
-            alignItems: "flex-start",
-            padding: 3
-          }}>
+          <Box
+            sx={{
+              width: "50%",
+              display: "flex",
+              justifyContent: "start",
+              alignItems: "flex-start",
+              padding: 3,
+            }}
+          >
             <img className="imagemLogo" src={logo} alt="logoWeg" />
           </Box>
           <Box
+            sx={{
+              width: "50%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-around",
+                alignItems: "center",
+                flexDirection: "column",
+                width: "60%",
+                height: "75%",
+                background: "rgba(255, 255, 255, 0.25)",
+                borderRadius: "5px",
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flexDirection: "column",
+                  color: "#FFF",
+                }}
+              >
+                <Typography sx={{ color: "#FFF" }} variant="h5">
+                  Bem vindo
+                </Typography>
+                <Typography sx={{ color: "#FFF" }} variant="body2">
+                  Sistema Sustentável de Organização de Demandas de TI
+                </Typography>
+              </Box>
+              <Box
+                sx={{
+                  width: "100%",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flexDirection: "column",
+                }}
+              >
+                <TextField
+                  sx={{ width: "70%", color: "#FFF", background: "#FFF" }}
+                  placeholder="Usuário"
+                  InputProps={{
+                    startAdornment: <AccountCircle />,
+                  }}
+                />
+                <TextField
+                  sx={{ width: "70%", color: "#FFF", background: "#FFF" }}
+                  placeholder="Senha"
+                  InputProps={{
+                    startAdornment: <LockRoundedIcon />,
+                    endAdornment: <RemoveRedEyeRoundedIcon />,
+                  }}
+                />
+                <Box
+                  sx={{
+                    width: "70%",
+                    display: "flex",
+                    justifyContent: "start",
+                    alignContent: "center",
+                    color: "#FFF",
+                  }}
+                >
+                  <Typography variant="body2">Esqueci minha senha</Typography>
+                </Box>
+              </Box>
+              <Box>
+                <Button
+                  sx={{
+                    width: "100%",
+                    backgroundColor: "#00579D",
+                    textDecoration: "none",
+                    color: "#fffffff",
+                  }}
+                  variant="contained"
+                  startIcon={<InputRoundedIcon />}
+                >
+                  <Link className="textBotao" onClick={colocaPessoa} to="/home">
+                    Entrar
+                  </Link>
+                </Button>
+              </Box>
+            </Box>
+          </Box>
+          {/* <Box
             sx={{
               width: "35%",
               height: "75%",
@@ -186,7 +278,7 @@ function Login(props: {
                 </Button>
               </Box>
             </Box>
-          </Box>
+          </Box> */}
         </Box>
         {/* Login */}
         {/* <Link onClick={colocaPessoa} to="/home">
