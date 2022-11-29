e37c1967b207f721466a194cdeb7e53ea9200294
DiegoPlaninscheck <92130153+DiegoPlaninscheck@users.noreply.github.com>
2022-11-28 21:55:40 -0300

    feat: tela de cadastro de demandas

--- src/Components/InformacaoGeral/InformacaoGeral.tsx
+++ src/Components/InformacaoGeral/InformacaoGeral.tsx
@@ -1,23 +1,58 @@
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import Autocomplete from '@mui/material/Autocomplete';
 
 export default function InformacaoGeral() {
-    <>
-        <Box>
-            <Box>
-                <Typography>
-                    Título:
-                </Typography>
-                <TextField />
-            </Box>
-            <Box>
-                <Typography>
-                    Problema a ser resolvido (situação atual):
-                </Typography>
 
-                 
+    const listaTeste = [
+        { label: 'teste 1' },
+        { label: 'teste 2' },
+        { label: 'teste 3' },
+        { label: 'teste 4' },
+        { label: 'teste 5' },
+    ]
+
+    return (
+        <>
+            <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", flexDirection: "column", width: "100%", minHeight: "70vh", maxHeight: "80vh" }}>
+                <Box sx={{ display: "flex", justifyContent: "center", flexDirection: "column", width: "100%", height: "25%" }}>
+                    <Typography>
+                        Título:
+                    </Typography>
+                    <TextField />
+                </Box>
+                <Box sx={{ display: "flex", justifyContent: "center", flexDirection: "column", width: "100%", height: "25%" }}>
+                    <Typography>
+                        Problema a ser resolvido (situação atual):
+                    </Typography>
+                    <TextField
+                        multiline
+                        rows={7}
+                        maxRows={Infinity} />
+                </Box>
+                <Box sx={{ display: "flex", justifyContent: "center", flexDirection: "column", width: "100%", height: "25%" }}>
+                    <Typography>
+                        Proposta / Solicitação de proposta:
+                    </Typography>
+                    <TextField
+                        multiline
+                        rows={7}
+                        maxRows={Infinity} />
+                </Box>
+                <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", width: "100%", height: "25%" }}>
+                    <Box sx={{ display: "flex", justifyContent: "center", flexDirection: "column", width: "100%", height: "100%" }}>
+                        <Typography>
+                            Centros de custo:
+                        </Typography>
+                        <Autocomplete
+                            multiple
+                            disablePortal
+                            options={listaTeste}
+                            renderInput={(params) => <TextField {...params} />} />
+                    </Box>
+                </Box>
             </Box>
-        </Box>
-    </>
+        </>
+    )
 }
--- src/Pages/TelaCriacaoDemanda/TelaCriacaoDemanda.tsx
+++ src/Pages/TelaCriacaoDemanda/TelaCriacaoDemanda.tsx
@@ -1,12 +1,37 @@
+import { useState } from "react";
 import Breadcrumb from "../../Components/Breadcrumb/Breadcrumb";
-import { BoxConteudo } from "../App.styles";
+import InformacaoGeral from "../../Components/InformacaoGeral/InformacaoGeral";
+import Box from '@mui/material/Box';
+import Container from "@mui/material/Container";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import PanoramaFishEyeRoundedIcon from '@mui/icons-material/PanoramaFishEyeRounded';
+import LensRoundedIcon from '@mui/icons-material/LensRounded';
+import { BotaoPrimario, BotaoTerciario, BoxConteudo } from "../App.styles";
+import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
 
 export default function TelaCriacaoDemanda() {
+
+    const [valor, setValor] = useState(0);
+
     return (
         <BoxConteudo>
-           <Breadcrumb />
-
-           
+            <Breadcrumb />
+            <Container sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginTop: 3, flexDirection: "column" }}>
+                <Tabs onChange={(e) => {
+                    console.log(e);
+                }}>
+                    /* Onde ta (azul), passou (cinza), não chegou (bolinha nao preenchida) */
+                    <Tab value={1} icon={<LensRoundedIcon sx={{ color: "#00579d" }} />}></Tab>
+                    <Tab value={2} icon={<PanoramaFishEyeRoundedIcon />}></Tab>
+                    <Tab value={3} icon={<PanoramaFishEyeRoundedIcon />}></Tab>
+                </Tabs>
+                <InformacaoGeral />
+                <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", width: "100%", marginTop: 5 }}>
+                    <BotaoTerciario sx={{ width: "15%", height: "3rem" }} variant='outlined'>Cancelar</BotaoTerciario>
+                    <BotaoPrimario sx={{ width: "15%", height: "3rem" }} variant='contained' endIcon={<ArrowForwardIosRoundedIcon sx={{ width: "15px" }} />}>Proximo</BotaoPrimario>
+                </Box>
+            </Container>
         </BoxConteudo>
     )
 }
