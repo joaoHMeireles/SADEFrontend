bd44552f42eda1b249fe4f90f188987d6e26c1fb
pessotticamilly <camilly_pessotti@estudante.sc.senai.br>
2022-12-01 16:15:01 -0300

    feat: esqueleto da página de perfil

--- src/Pages/Perfil/Perfil.tsx
+++ src/Pages/Perfil/Perfil.tsx
@@ -1,12 +1,47 @@
 import "./Perfil.scss";
 
 import Breadcrumb from "../../Components/Breadcrumb/Breadcrumb";
-import { BoxContainer, BoxConteudo, BotaoTerciario, BotaoPrimario } from "../App.styles"
+import { BoxContainer, BoxConteudo, BotaoTerciario, BotaoPrimario } from "../App.styles";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import fotoPerfil from "../../Assets/fotoPerfil.jpg"
 
 export default function Perfil() {
     return (
         <BoxConteudo>
             <Breadcrumb />
-        </BoxConteudo>
+
+            <div id="container">
+                <div id="panel">
+                    <div className="row">
+                        <div className="text">
+                            <p>Foto de perfil</p>
+                        </div>
+
+                        <div id="img">
+                            <img src={fotoPerfil} alt="Foto de perfil" />
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="text">
+                            <p>Usuário</p>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="text">
+                            <p>Email</p>
+                        </div>
+                    </div>
+
+                    <div className="row" id="last">
+                        <div className="text">
+                            <p>Senha</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </BoxConteudo >
     )
 }
