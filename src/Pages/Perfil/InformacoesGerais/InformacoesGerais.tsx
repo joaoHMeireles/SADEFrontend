f7278a51f87388582bff99b8740b0baa2a9b9321
Camilly Pessotti <camilly_pessotti@estudante.sc.senai.br>
2022-12-16 12:27:16 -0300

    feat: fazendo a segunda aba do perfil

--- src/Pages/Perfil/InformacoesGerais/InformacoesGerais.tsx
+++ src/Pages/Perfil/InformacoesGerais/InformacoesGerais.tsx
@@ -31,7 +31,7 @@ export default function InformacoesGerais() {
         </h4>
 
         <p>
-          Romário Hornburg
+          Benson Rodrigues
         </p>
 
         <h4>
@@ -39,7 +39,7 @@ export default function InformacoesGerais() {
         </h4>
 
         <p>
-          romario_hornburg@weg.net
+          benson_rodrigues@weg.net
         </p>
       </Box>
 
@@ -51,7 +51,7 @@ export default function InformacoesGerais() {
         </h4>
 
         <p>
-          Nome do departamento
+          RH
         </p>
 
         <h4>
@@ -59,7 +59,7 @@ export default function InformacoesGerais() {
         </h4>
 
         <p>
-          Nome do setor
+          Treinamento
         </p>
       </Box>
     </Box>
