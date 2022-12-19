0eb3211f93d75ea7aa55faf1be5afba851323495
pessotticamilly <camilly_pessotti@estudante.sc.senai.br>
2022-12-19 16:46:08 -0300

    refactor: estilizando o gráfico de novo

--- src/Pages/Perfil/MeuDesempenho/MeuDesempenho.tsx
+++ src/Pages/Perfil/MeuDesempenho/MeuDesempenho.tsx
@@ -12,7 +12,7 @@ const data1 = {
     label: "Demandas",
     data: [1, 3, 2, 1],
     backgroundColor: ["#00579D", "#5B93BF", "#95B9D5", "#FFF"],
-    borderColor: ["#eee"],
+    borderColor: ["#EEE"],
     borderWidth: 1
   }]
 }
@@ -33,9 +33,9 @@ const options1: _DeepPartialObject<PluginChartOptions<"doughnut">> = {
 export default function MeuDesempenho() {
   return (
     <Container sx={{ alignItems: "center", display: "flex", justifyContent: "space-between" }}>
-      <Box sx={{ backgroundColor: "#EEE", height: "auto", padding: "0 1rem 0 0", width: "auto" }}>
+      <Box sx={{ backgroundColor: "#EEE", borderRadius: "1rem", boxShadow: "5px 5px 10px 0 #00000050", height: "auto", padding: "1rem 2rem 1rem 1rem", width: "auto" }}>
         <Box sx={{ height: "auto", width: "20vw" }}>
-          <Doughnut data={data1} options={options1} />
+          <Doughnut data={data1} options={options1}  />
         </Box>
       </Box>
     </Container>
