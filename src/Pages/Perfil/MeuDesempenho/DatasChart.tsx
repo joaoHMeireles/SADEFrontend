76309b6952b4a0ee37071fa48455241c747b39b4
Camilly de Souza Pessotti <camilly_pessotti@estudante.sc.senai.br>
2023-02-01 20:22:18 -0300

    refactor: ainda nos gráficos

--- src/Pages/Perfil/MeuDesempenho/DatasChart.tsx
+++ src/Pages/Perfil/MeuDesempenho/DatasChart.tsx
@@ -1,3 +1,5 @@
+import faker from 'faker';
+
 export const dataLine1 = {
     labels: ["", "", "", "", "", "", "", "", "", ""],
     datasets: [{
@@ -161,22 +163,72 @@ export const pluginsDoughnut = {
     }
 };
 
-export const dataHorizontalBar = {
-    datasets: [{
-        label: "Entregas",
-        data: [2, 4, 1],
-        backgroundColor: ["#00579D", "#5B93BF", "#95B9D5"]
-    }]
-};
+// const labels = ["", "", "", "", "", "", ""];
+
+// export const dataHorizontalBar = {
+//   labels: labels,
+//   datasets: [{
+//     data: [1, 4, 2],
+//     backgroundColor: ["#00579D", "#5B93BF", "#95B9D5"],
+//   }],
+//   type: 'bar',
+//   options: {
+//     scales: {
+//       y: {
+//         beginAtZero: true
+//       }
+//     }
+//   },
+// };
+
+// export const configHorizontalBar = {
+//   type: 'bar',
+//   data: dataHorizontalBar,
+//   options: {
+//     scales: {
+//       y: {
+//         beginAtZero: true
+//       }
+//     }
+//   },
+// };
 
 export const optionsHorizontalBar = {
-    type: 'bar',
-    dataHorizontalBar,
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-};
+    indexAxis: 'y' as const,
+    elements: {
+      bar: {
+        borderWidth: 2,
+      },
+    },
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'right' as const,
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Horizontal Bar Chart',
+      },
+    },
+  };
+  
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  
+  export const dataHorizontalBar = {
+    labels,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: 'Dataset 2',
+        data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
+  
