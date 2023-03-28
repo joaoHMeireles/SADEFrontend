5223579747f33df6933e3c88dad7e73f04559261
Camilly de Souza Pessotti <camilly_pessotti@estudante.sc.senai.br>
2023-03-28 17:28:11 -0300

    Refactor: Fazendo os tooltips da página de criar demanda

--- src/Components/InformacaoGeral/InformacaoGeral.tsx
+++ src/Components/InformacaoGeral/InformacaoGeral.tsx
@@ -14,12 +14,25 @@ import {
 } from "./InformacaoGeral.styles";
 import Checkbox from "@mui/material/Checkbox";
 import api from "../../api/api";
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import { Box, Button, ClickAwayListener } from "@mui/material";
 
 export default function InformacaoGeral(props: { proposta: boolean, centroCusto?: any[], setCentroCusto?: React.Dispatch<React.SetStateAction<Object[]>> }) {
   // const info = JSON.parse(localStorage.getItem("RASCUNHOESCOLHIDO") as string);
-
+  const [paginaTooltip, setPaginaTooltip] = useState(0);
   const [centroCusto, setCentroCusto] = useState<any[]>([]);
   const [idCentroCusto, setIdCentroCusto] = useState<any[]>([]);
+  const [open, setOpen] = useState(false);
+
+  const handleTooltipClose = () => {
+    setOpen(false);
+  };
+
+  const handleTooltipOpen = () => {
+    setOpen(true);
+  };
 
   const demandaSelecionada = JSON.parse(
     localStorage.getItem("DEMANDASELECIONADA") as string
@@ -72,7 +85,62 @@ export default function InformacaoGeral(props: { proposta: boolean, centroCusto?
     <>
       <BoxContainerGeralInformacaoGeral>
         <BoxContainerLabels>
-          <TypographyLabels>Título:</TypographyLabels>
+          <TypographyLabels>
+            <ClickAwayListener onClickAway={handleTooltipClose}>
+              <div>
+                <Tooltip
+                  PopperProps={{
+                    disablePortal: true,
+                  }}
+                  open={open}
+                  disableFocusListener
+                  disableHoverListener
+                  disableTouchListener
+                  title={
+                    <>
+                      <Box sx={{ width: "200px", height: "150px" }}>
+                        {paginaTooltip == 0 ?
+                          <Box>
+                            informações sobre o titulo e o que deveria ser colocado
+                          </Box>
+                          : paginaTooltip == 1 ?
+                            <Box>
+                              informações sobre oproblema a ser resolvido
+                            </Box>
+                            : paginaTooltip == 2 ?
+                              <Box>
+                                informações sobre a proposta e tudo mais
+                              </Box>
+                              :
+                              <Box>
+                                informações sobre os centros de custo
+                              </Box>
+                        }
+                        <Box color={"white"}>
+                          {paginaTooltip > 0 &&
+                            <Button onClick={() => { setPaginaTooltip(paginaTooltip - 1) }} sx={{ color: "#ffffff" }}>
+                              Voltar
+                            </Button>
+                          }
+                          {paginaTooltip < 3 &&
+                            <Button onClick={() => { setPaginaTooltip(paginaTooltip + 1) }} sx={{ color: "#ffffff" }}>
+                              Próximo
+                            </Button>
+                          }
+                        </Box>
+                      </Box>
+                    </>
+                  }
+                >
+                  <IconButton onClick={handleTooltipOpen}>
+                    <HelpOutlineIcon fontSize="small" />
+                  </IconButton>
+                </Tooltip>
+              </div>
+            </ClickAwayListener>
+
+            Título:
+          </TypographyLabels>
           <TextField
             id="titulo"
             sx={{ boxShadow: "5px 5px 10px 0 #00000050" }}
@@ -80,6 +148,11 @@ export default function InformacaoGeral(props: { proposta: boolean, centroCusto?
         </BoxContainerLabels>
         <BoxContainerLabels>
           <TypographyLabels>
+            <Tooltip title="Escreva uma breve explicação da situação atual, descrevendo o problema em mais detalhes. Forneça informações relevantes e quantitativas, se possível, para apoiar sua argumentação">
+              <IconButton>
+                <HelpOutlineIcon fontSize="small" />
+              </IconButton>
+            </Tooltip>
             Problema a ser resolvido (situação atual):
           </TypographyLabels>
           <TextField
@@ -91,6 +164,11 @@ export default function InformacaoGeral(props: { proposta: boolean, centroCusto?
         </BoxContainerLabels>
         <BoxContainerLabels>
           <TypographyLabels>
+            <Tooltip title="Proponha uma solução ou solicite uma proposta para resolver o problema. Se você já tiver uma ideia de como resolver o problema, descreva-a em detalhes. Se você não tiver uma solução, solicite propostas de soluções de outras pessoas ou organizações">
+              <IconButton>
+                <HelpOutlineIcon fontSize="small" />
+              </IconButton>
+            </Tooltip>
             Proposta / Solicitação de proposta:
           </TypographyLabels>
           <TextField
@@ -102,7 +180,14 @@ export default function InformacaoGeral(props: { proposta: boolean, centroCusto?
         </BoxContainerLabels>
         <BoxContainerLabels>
           <BoxContainerCentroCusto>
-            <TypographyLabels>Centros de custo:</TypographyLabels>
+            <TypographyLabels>
+              <Tooltip title="Identifique os centros de custo envolvidos na solução do problema. Isso pode incluir recursos financeiros, materiais ou humanos necessários para implementar a solução">
+                <IconButton>
+                  <HelpOutlineIcon fontSize="small" />
+                </IconButton>
+              </Tooltip>
+              Centros de custo:
+            </TypographyLabels>
             {props.proposta ? (
               <Autocomplete
                 id="centrosDeCusto"
