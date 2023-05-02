4e3e6e266d203197cb148d68d06c751cd0880bc2
Camilly de Souza Pessotti <camilly_pessotti@estudante.sc.senai.br>
2023-05-02 19:23:09 -0300

    refactor: arrumando a lista

--- src/Pages/AjudaUsuario/AjudaUsuario.tsx
+++ src/Pages/AjudaUsuario/AjudaUsuario.tsx
@@ -26,8 +26,8 @@ export default function AjudaUsuario(props: { aberto: boolean, sidebarAberta: bo
   };
 
   return (
-    <Box sx={{ backgroundColor: "#EEE" }}>
-      <BoxHeader sx={{ backgroundColor: "#EEE", width: (props.sidebarAberta ? "88.35%" : "96.5%") }}>
+    <Box>
+      <BoxHeader sx={{ width: (props.sidebarAberta ? "88.35%" : "96.5%") }}>
         <Breadcrumb />
       </BoxHeader>
 
@@ -36,7 +36,7 @@ export default function AjudaUsuario(props: { aberto: boolean, sidebarAberta: bo
       <BoxConteudo>
         <Container>
           <FirstColumn>
-          <Lista sx={{ backgroundColor: "#FFF" }}>
+          <Lista>
             <ListItemButton>
               <ListItemText primary="INTRODUÇÃO" />
             </ListItemButton>
