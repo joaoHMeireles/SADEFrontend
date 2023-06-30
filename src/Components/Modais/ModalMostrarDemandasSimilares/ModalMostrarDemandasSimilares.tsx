ebb42261724e892463cc26dd593148c0cae7be13
Diego <92130153+DiegoPlaninscheck@users.noreply.github.com>
2023-06-30 19:24:07 -0300

    feat: modal feito
    terminar modal quanto tiver as demandas similares funcionando

--- src/Components/Modais/ModalMostrarDemandasSimilares/ModalMostrarDemandasSimilares.tsx
+++ src/Components/Modais/ModalMostrarDemandasSimilares/ModalMostrarDemandasSimilares.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import { TextReaderContext } from "../../TextReaderContext/TextReaderContext"
 import { Modal } from "../../../constants/interfaces"
 import { Alert, Box, IconButton, TextField, Typography } from "@mui/material"
@@ -7,12 +7,19 @@ import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 import ReportProblemRoundedIcon from '@mui/icons-material/ReportProblemRounded';
 import { BoxBotoesModal, BoxConteudoModal, BoxTituloModal, TypographyTituloModal } from "../../../Pages/TelaProcesso/TelaProcesso.styles";
 import { BotaoPrimario, BotaoSecundario } from "../../../Pages/App.styles";
+import { BoxAtencaoModalSimilaridade, BoxBotoesModalDemandaSimilar, BoxConteudoModalDemandaSimilaridade, BoxIconeFecharModal, BoxLinkDemandasSimilares, TypographyAtencaoDemandaSimilar, TypographyFraseDemandaSimilar, TypographyLinkDemandaSimilar, TypographyLinkDemandasSimilar } from "../Modais.style";
 
 
-export default function ModalMostrarDemandasSimilares(props: {open: any, setOpen: any}) {
+export default function ModalMostrarDemandasSimilares(props: { open: any, setOpen: any }) {
     let { lerTexto } = useContext(TextReaderContext) as any
     const [erroMotivoDevolucao, setErroMotivoDevolucao] = useState({ error: false, helperText: "" })
     const [demandasSimilares, setDemandaSimilares] = useState<any>([1, 2])
+    const [modalOpen, setModalOpen] = useState<any>(true);
+
+    useEffect(() => {
+        props.setOpen(modalOpen)
+    }, [modalOpen])
+
     // const conteudoFeedback = (
     //     <Alert onClose={() => { props.setFeedbackAberto(false) }} severity="success" sx={{ width: '100%' }} onClick={lerTexto}>
     //         Motivo da devolução enviado
@@ -41,60 +48,58 @@ export default function ModalMostrarDemandasSimilares(props: {open: any, setOpen
     return (
         <BoxConteudoModal>
 
-            <Box sx={{ width: "100%", display: "flex", justifyContent: "flex-end", alignItems: "center" }}>
-                <CloseRoundedIcon sx={{ color: "#444", cursor: "pointer" }} 
-                // onClick={props.setOpen(!props.open)}
+            <BoxIconeFecharModal>
+                <CloseRoundedIcon sx={{ color: "#444", cursor: "pointer" }}
+                    onClick={() => {
+                        setModalOpen(false)
+                    }}
                 />
-            </Box>
+            </BoxIconeFecharModal>
 
-            <Box sx={{ width: "100%", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
-                <Typography sx={{ color: "#00579D", fontSize: "20px", fontWeight: "bold" }} variant="h6">Atenção</Typography>
-                <ReportProblemRoundedIcon sx={{ color: "#00579d", width: "80px", height: "80px", marginTop: 2 }} />
-            </Box>
+            <BoxAtencaoModalSimilaridade>
+                <TypographyAtencaoDemandaSimilar variant="h6">Atenção</TypographyAtencaoDemandaSimilar>
+                <ReportProblemRoundedIcon sx={{ color: "#00579d", width: "80px", height: "80px", marginY: 2 }} />
+            </BoxAtencaoModalSimilaridade>
 
-            <Box sx={{ width: "100%", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+            <BoxConteudoModalDemandaSimilaridade>
                 {demandasSimilares.length > 1 ?
                     <>
-                        <Typography sx={{ textAlign: "center", marginTop: 2 }}>
+                        <TypographyFraseDemandaSimilar>
                             Foram encontradas {demandasSimilares.length} demandas similar a sua.
                             Para evitar redundância em nosso sistema, avalie a demanda abaixo.
                             Caso a problemática ou a solução seja diferente, prossiga com seu cadastro.
-                        </Typography>
-                        <Box sx={{
-                            width: "80%", maxHeight: "10vh", display: "flex", justifyContent: "flex-start", alignItems: "center",
-                            flexDirection: "column", overflowX: "hidden", overflowY: "auto", marginY: 3, '&::-webkit-scrollbar': { backgroundColor: "transparent", }
-                        }}>
-                            <Typography sx={{ textDecoration: "underline", color: "#00579d", cursor: "pointer", marginBottom: 1 }}>Ver demanda</Typography>
-                            <Typography sx={{ textDecoration: "underline", color: "#00579d", cursor: "pointer", marginBottom: 1 }}>Ver demanda um pouco maior</Typography>
-                            <Typography sx={{ textDecoration: "underline", color: "#00579d", cursor: "pointer", marginBottom: 1 }}>Ver demanda</Typography>
-                            <Typography sx={{ textDecoration: "underline", color: "#00579d", cursor: "pointer", marginBottom: 1 }}>Ver demanda um pouco maior, tipo vezes 2</Typography>
-                            <Typography sx={{ textDecoration: "underline", color: "#00579d", cursor: "pointer", marginBottom: 1 }}>Ver demanda</Typography>
-                        </Box>
+                        </TypographyFraseDemandaSimilar>
+                        <BoxLinkDemandasSimilares>
+                            <TypographyLinkDemandasSimilar>Ver demanda</TypographyLinkDemandasSimilar>
+                        </BoxLinkDemandasSimilares>
                     </>
                     :
                     <>
-                        <Typography sx={{ textAlign: "center", marginTop: 2 }}>
+                        <TypographyFraseDemandaSimilar>
                             Foi encontrada uma demanda similar a sua.
                             Para evitar redundância em nosso sistema, avalie a demanda abaixo.
                             Caso a problemática ou a solução seja diferente, prossiga com seu cadastro.
-                        </Typography>
-                        <Typography sx={{ textDecoration: "underline", marginY: 4, color: "#00579d", cursor: "pointer" }}>Ver demanda</Typography>
+                        </TypographyFraseDemandaSimilar>
+                        <TypographyLinkDemandaSimilar>Ver demanda</TypographyLinkDemandaSimilar>
                     </>
                 }
 
-            </Box>
+            </BoxConteudoModalDemandaSimilaridade>
 
-            <Box sx={{ display: "flex", justifyContent: "space-around", alignItems: "center" }}>
+            <BoxBotoesModalDemandaSimilar>
                 <BotaoSecundario onClick={(e: any) => {
                     lerTexto(e);
                     // props.fecharModal(e) 
                 }} variant='outlined'>
                     Continuar
                 </BotaoSecundario>
-                <BotaoPrimario onClick={(e: any) => { lerTexto(e); finalizarAcao() }} variant="contained">
+                <BotaoPrimario onClick={(e: any) => {
+                    lerTexto(e)
+                    setModalOpen(false)
+                }} variant="contained">
                     Cancelar
                 </BotaoPrimario>
-            </Box>
+            </BoxBotoesModalDemandaSimilar>
         </BoxConteudoModal>
     )
 }
